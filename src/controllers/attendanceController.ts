aeb85b178194b2326fec64483dc7b4c4356b5e75
Puskar-Roy <puskarroy600@gmail.com>
2024-03-21 19:59:07 +0530

    feat : change attendance

--- src/controllers/attendanceController.ts
+++ src/controllers/attendanceController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import asyncHandler from '../util/catchAsync';
 import AttendanceModel from '../models/attendanceSchema';
+import UserModel from '../models/userSchema';
 
 export const createAttendance = asyncHandler(
   async (req: Request, res: Response) => {
@@ -39,29 +40,6 @@ export const getAttendance = asyncHandler(
     }
   }
 );
-// export const getAttendancewithDate = asyncHandler(
-//   async (req: Request, res: Response) => {
-//     try {
-//       const { date } = req.params;
-
-//       if (!date) {
-//         return res.status(400).json({ message: 'Date parameter is required.' });
-//       }
-
-//       const attendance = await AttendanceModel.find({
-//         date: {
-//           $gte: new Date(date),
-//           $lt: new Date(date).setDate(new Date(date).getDate() + 1),
-//         },
-//       }).exec();
-
-//       res.status(200).send(attendance);
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).send('Server Error');
-//     }
-//   }
-// );
 
 export const getAttendancewithDate = asyncHandler(
   async (req: Request, res: Response) => {
@@ -106,6 +84,62 @@ export const getAttendanceCounts = asyncHandler(
     } catch (err) {
       console.error(err);
       res.status(500).send('Server Error');
+    }
+  }
+);
+
+export const getSingleAttendance = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    console.log(id);
+
+    try {
+      const attendance = await AttendanceModel.findById(id);
+
+      if (!attendance) {
+        return res.status(404).json({ message: 'Attendance not found' });
+      }
+
+      const { userId, date, status } = attendance;
+
+      const user = await UserModel.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      const { name, email } = user;
+
+      res
+        .status(200)
+        .json({ status: status, userName: name, userEmail: email, date });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
+    }
+  }
+);
+
+export const changeAttendance = asyncHandler(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    try {
+      const attendance = await AttendanceModel.findByIdAndUpdate(
+        id,
+        { status },
+        { new: true }
+      );
+
+      if (!attendance) {
+        return res.status(404).json({ message: 'Attendance not found' });
+      }
+
+      res.status(200).json(attendance);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Server Error' });
     }
   }
 );
--- src/routes/attendanceRoutes.ts
+++ src/routes/attendanceRoutes.ts
@@ -4,6 +4,8 @@ import {
   getAttendance,
   getAttendancewithDate,
   getAttendanceCounts,
+  getSingleAttendance,
+  changeAttendance,
 } from '../controllers/attendanceController';
 import { protect, protectAdmin } from '../middleware/middleware';
 
@@ -11,7 +13,9 @@ const router: Router = express.Router();
 
 router.post('/attendance', protect, createAttendance);
 router.get('/attendance/:userId', protectAdmin, getAttendance);
+router.get('/attendance/user/:id', protectAdmin, getSingleAttendance);
 router.get('/attendance/date/:date', protectAdmin, getAttendancewithDate);
 router.get('/attendance/counts/:userId', protect, getAttendanceCounts);
+router.put('/attendance/:id/change-status', protect, changeAttendance);
 
 export default router;
